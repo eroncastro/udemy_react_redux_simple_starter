20d4b8d2afac6a003dad3394b9199dbd0df04bd0
Eron <eroncastro@gmail.com>
2017-03-16 08:18:12 -0300

    Makes component render properly

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom'; // Used to manage the DOM
 
 // In React, components are collections of JS code that produce HTML.
 // Create a new component. This component should produce some HTML.
-const App = function() {
+const App = () => {
   return <div>Hi!</div>; // JSX: dialect of JavaScript that looks like HTML.
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM).
-ReactDOM.render(<App />);
+ReactDOM.render(<App />, document.querySelector('.container'));
 
