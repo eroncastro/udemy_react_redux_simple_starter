22820b26eff6978caffa467705f5541d9338c1a9
Eron <eroncastro@gmail.com>
2017-03-16 09:09:32 -0300

    Refactores functional component and a class based component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-  return <input />;
-};
+/*
+  Functional component: function that returns JSX
+  const SearchBar = () => {
+    return <input />;
+  };
+*/
+
+// Class based component
+class SearchBar extends Component {
+  // Every class based component must have a render method what will return JSX.
+  render() {
+    return <input />;
+  }
+}
+
 
 export default SearchBar;
