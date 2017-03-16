36965c3d7955382319b298c99b9016b18f94c53a
Eron <eroncastro@gmail.com>
2017-03-16 08:13:03 -0300

    Fixes imports and tries to pass an instance ReactDOM.render

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import React from 'react'; // Used to create components
+import ReactDOM from 'react-dom'; // Used to manage the DOM
+
 // In React, components are collections of JS code that produce HTML.
 // Create a new component. This component should produce some HTML.
-const App = () => {
+const App = function() {
   return <div>Hi!</div>; // JSX: dialect of JavaScript that looks like HTML.
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM).
+ReactDOM.render(<App />);
 
