08bd98dc8fe3b59224e4b7d5a3002f9095390e52
Eron <eroncastro@gmail.com>
2017-03-17 07:27:32 -0300

    Adds event handler for input element

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 /*
   Functional component: function that returns JSX
@@ -7,13 +7,27 @@ import React, { Component } from 'react';
   };
 */
 
+/*
+  Handling an event in React has two steps:
+  1) We declare an event handler, which is a function that should run whenever the event occurs;
+  2) We pass the event handler to the element we want to monitor for events.
+*/
+
 // Class based component
-class SearchBar extends Component {
+class SearchBar extends React.Component {
   // Every class based component must have a render method what will return JSX.
   render() {
-    return <input />;
+    // camelcase([on + [event name]])
+    return <input onChange={event => console.log(event.target.value)} />;
   }
-}
 
+  /*
+    Syntax convention for event handler:
+    camelCase([handle or on] + [element name] + [event name])
+  */
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
+}
 
 export default SearchBar;
