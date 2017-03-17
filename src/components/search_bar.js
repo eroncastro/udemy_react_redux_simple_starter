813416561fd3536ebd8c084f808153b0eb79924d
Eron <eroncastro@gmail.com>
2017-03-17 07:49:28 -0300

    Changes component state whenever input value changes

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,10 +5,15 @@ import React from 'react';
   const SearchBar = () => {
     return <input />;
   };
+*/
 
+/*
   Handling an event in React has two steps:
   1) We declare an event handler, which is a function that should run whenever the event occurs;
+  2) We pass the event handler to the element we want to monitor for events.
+*/
 
+/*
   In React, state is a plain JavaScript object that is used to record and react to user events.
   Each class based component has its own state object. Whenever a component's state changes,
   the component re-renders and forces all of its children to re-render as well.
@@ -16,6 +21,10 @@ import React from 'react';
   Before we ever use state inside of a component, we need to initialize the state object.
   To initiliaze state, we set the property state to a plain JavaScript object inside of the
   class constructor method. Functional components do not have state, only class based components do.
+*/
+
+/*
+  To change our component state, we use this.setState method.
 */
 
 // Class based component
@@ -28,7 +37,11 @@ class SearchBar extends React.Component {
   // Every class based component must have a render method what will return JSX.
   render() {
     // camelcase([on + [event name]])
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    );
   }
 
   /*
