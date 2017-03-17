aaea9e3608691461ae32b875bb9c984e7687610a
Eron <eroncastro@gmail.com>
2017-03-17 07:36:52 -0300

    Defines object initial state inside component class constructor

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,16 +5,26 @@ import React from 'react';
   const SearchBar = () => {
     return <input />;
   };
-*/
 
-/*
   Handling an event in React has two steps:
   1) We declare an event handler, which is a function that should run whenever the event occurs;
-  2) We pass the event handler to the element we want to monitor for events.
+
+  In React, state is a plain JavaScript object that is used to record and react to user events.
+  Each class based component has its own state object. Whenever a component's state changes,
+  the component re-renders and forces all of its children to re-render as well.
+
+  Before we ever use state inside of a component, we need to initialize the state object.
+  To initiliaze state, we set the property state to a plain JavaScript object inside of the
+  class constructor method. Functional components do not have state, only class based components do.
 */
 
 // Class based component
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' }; // This is how we initialize state inside a class based component.
+  }
   // Every class based component must have a render method what will return JSX.
   render() {
     // camelcase([on + [event name]])
