f9ad996f7ef39efc3980cd8cbae59be7630cac15
Eron <eroncastro@gmail.com>
2017-03-20 08:40:21 -0300

    Removes blank line

--- src/index.js
+++ src/index.js
@@ -68,4 +68,3 @@ class App extends React.Component {
 
 // Take this component's generated HTML and put it on the page (in the DOM).
 ReactDOM.render(<App />, document.querySelector('.container'));
-
